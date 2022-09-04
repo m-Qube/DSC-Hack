2375fc4508b6d6004d6e9d3d835aff8e3e0864c4
ayushkumar05 <ayushk0520@gmail.com>
2022-09-04 10:19:33 +0530

    opencv

--- ds01/website/auth.py
+++ ds01/website/auth.py
@@ -1,6 +1,86 @@
 from email import message
-from flask import Blueprint, render_template, request, redirect, session, url_for
+from flask import Blueprint, render_template, request, redirect, session, url_for,Response
 import pymongo, bcrypt
+#from att import out
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+
+# from PIL import ImageGrab
+
+path = '/home/ayush/Desktop/DSC-Hack/ds01/website/Training_images'
+images = []
+classNames = []
+myList = os.listdir(path)
+#print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+#print(classNames)
+
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+
+def markAttendance(name):
+    with open('Attendance.csv', 'r+') as f:
+        myDataList = f.readlines()
+
+        nameList = []
+        for line in myDataList:
+            entry = line.split(',')
+            nameList.append(entry[0])
+            if name not in nameList:
+                now = datetime.now()
+                dtString = now.strftime('%H:%M:%S')
+                f.writelines(f'\n{name},{dtString}')
+
+encodeListKnown = findEncodings(images)
+#print('Encoding Complete')
+
+#cap = cv2.VideoCapture(0)
+#cap.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
+#cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
+
+def out(cap):
+    #while True:
+        cv2.waitKey(1)
+        success, img = cap.read()
+        imgS = cv2.resize(img, (0,0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+
+        facesCurFrame = face_recognition.face_locations(imgS)
+        encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+
+        for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                ret, jpeg = cv2.imencode('.jpg', img)
+                return jpeg.tobytes(),[True,name]
+                #markAttendance(name)
+        
+        ret, jpeg = cv2.imencode('.jpg', img)
+        #cv2.imshow('Webcam', img)
+        
+        return jpeg.tobytes(),[False,'n/a']
 
 auth = Blueprint('auth', __name__)
 
@@ -100,3 +180,26 @@ def cam():
 @auth.route('/attendance')
 def att():
     return render_template('attendance.html')
+
+def gen():
+    cap = cv2.VideoCapture(0)
+    while True:
+        try:
+            frame,marked = out(cap)
+            if marked:
+                print(marked)
+        except:
+            frame,marked = out(cap)
+            print(marked)
+
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        if marked[0]==True:
+            global m
+            m= marked
+            
+            
+        
+
+@auth.route('/video_feed')
+def video_feed():
+    return Response(gen(),mimetype='multipart/x-mixed-replace; boundary=frame')
